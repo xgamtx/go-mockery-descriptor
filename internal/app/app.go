13d040a45ec4b9388c4ea7fdb425af8e31072991
vasilij <klunew@yandex.ru>
2026-02-08 18:01:50 +0300

    add config discover

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,18 +1,19 @@
 package app
 
 import (
+	"github.com/xgamtx/go-mockery-descriptor/internal/config"
 	"github.com/xgamtx/go-mockery-descriptor/internal/fieldoverwriter"
 	"github.com/xgamtx/go-mockery-descriptor/internal/generator"
 	"github.com/xgamtx/go-mockery-descriptor/internal/parser"
 )
 
-func Run(dir, interfaceName string, fieldOverwriterParams []string) (string, error) {
-	desc, err := parser.ParseInterfaceInDir(dir, interfaceName)
+func Run(cfg *config.Config) (string, error) {
+	desc, err := parser.ParseInterfaceInDir(cfg.Dir, cfg.Interface)
 	if err != nil {
 		return "", err
 	}
 
-	overwriterStorage, err := fieldoverwriter.NewStorage(fieldOverwriterParams)
+	overwriterStorage, err := fieldoverwriter.NewStorage(cfg.FieldOverwriterParams)
 	if err != nil {
 		return "", err
 	}
---
+++ internal/config/config.go
@@ -0,0 +1,86 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Dir                   string   `mapstructure:"dir"`
+	Interface             string   `mapstructure:"interface"`
+	Output                string   `mapstructure:"output"`
+	FieldOverwriterParams []string `mapstructure:"field-overwriter-param"`
+}
+
+func initFlags() {
+	pflag.String("dir", "", "output directory")
+	pflag.String("interface", "", "interface name")
+	pflag.String("output", "", "output file")
+	pflag.StringSlice("field-overwriter-param", nil, "field overwriter param, can be used more than once")
+
+	pflag.Parse()
+}
+
+func New() (*Config, error) {
+	initFlags()
+	viper.SetConfigType("yaml")
+
+	dirs, err := getConfigPaths()
+	if err != nil {
+		return nil, err
+	}
+
+	for i := len(dirs) - 1; i >= 0; i-- {
+		dir := dirs[i]
+		candidate := filepath.Join(dir, ".mockery-descriptor.yaml")
+		if _, err = os.Stat(candidate); os.IsNotExist(err) {
+			continue
+		}
+
+		viper.SetConfigFile(candidate)
+		if err = viper.MergeInConfig(); err != nil {
+			return nil, err
+		}
+	}
+
+	if err = viper.BindPFlags(pflag.CommandLine); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err = viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
+func getConfigPaths() ([]string, error) {
+	var paths []string
+
+	current, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	paths = append(paths, current)
+	for {
+		modPath := filepath.Join(current, "go.mod")
+		if _, err = os.Stat(modPath); err == nil {
+			break
+		}
+
+		parent := filepath.Dir(current)
+		if parent == current {
+			break
+		}
+
+		paths = append(paths, parent)
+		current = parent
+	}
+
+	return paths, nil
+}
